0f1f93b1dead062c22cdc19d71650aab0f378a51
ansh111 <anshulnigam111@gmail.com>
2025-11-22 17:09:33 +0530

    deleted video player files

--- app/src/main/java/com/anshul/expenseai/ui/compose/videoai/state/VideoSummarisationSideEffect.kt
+++
@@ -1,6 +0,0 @@
-package com.anshul.expenseai.ui.compose.videoai.state
-
-sealed class VideoSummarisationSideEffect {
-    data class ShowToast(val message: String) : VideoSummarisationSideEffect()
-    data class NavigateToDetails(val videoId: String) : VideoSummarisationSideEffect()
-}
